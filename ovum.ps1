02e9a40ba07e2a8d23506480346c71489976ccff
scarytom <t.denley@cantab.net>
2012-04-28 22:49:05 +0100

    progressing windows script

--- ovum.ps1
+++ ovum.ps1
@@ -1,2 +1,19 @@
-Write-Host "Not yet supported1"
-Write-Host "Not yet supported2"
+if ($args.Count -ne 2)
+{
+  Write-Host "Usage: ovum.ps1 lang projectdir"
+  break
+}
+
+$parent = args[0]
+$womb = args[1]
+$ovary = $env:temp\[System.IO.Path]::GetRandomFileName()
+
+Directory.CreateDirectory($ovary);
+(new-object System.Net.WebClient).DownloadFile("https://nodeload.github.com/scarytom/ovum/zipball/$parent","$ovary\data.zip")
+
+$shell_app=new-object -com shell.application
+$zip_file = $shell_app.namespace("$ovary\data.zip")
+$destination = $shell_app.namespace("$ovary")
+$destination.Copyhere($zip_file.items())
+
+Write-Host $ovary
