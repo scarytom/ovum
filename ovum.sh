acb0e0b4ef5859c45be3f0fbeff9b00e3f45b301
scarytom <t.denley@cantab.net>
2012-04-28 18:42:13 +0100

    no overwrite

--- ovum.sh
+++ ovum.sh
@@ -8,5 +8,5 @@ OVARY=`mktemp -d`
 
 mkdir -p "$WOMB"
 wget --no-check-certificate "https://nodeload.github.com/scarytom/ovum/tarball/$PARENT" -O - | tar xz -C "$OVARY"
-find "$OVARY"/*/. -mindepth 1 -maxdepth 1 -exec mv -t"$WOMB"/. -- {} +
+find "$OVARY"/*/. -mindepth 1 -maxdepth 1 -exec mv -n -t"$WOMB"/. -- {} +
 rmdir "$OVARY"/* "$OVARY"
