874cc26837ad18db80160cce53529bb751b02c81
scarytom <t.denley@cantab.net>
2012-04-28 23:27:58 +0100

    finish up the Windows powershell script

--- ovum.ps1
+++ ovum.ps1
@@ -4,16 +4,31 @@ if ($args.Count -ne 2)
   break
 }
 
-$parent = args[0]
-$womb = args[1]
-$ovary = $env:temp\[System.IO.Path]::GetRandomFileName()
+$ErrorActionPreference='Stop'
+$parent = $args[0]
+$womb = $args[1]
+$ovary = "$env:temp\" + [System.IO.Path]::GetRandomFileName()
 
-Directory.CreateDirectory($ovary);
+Write-Host "working in $ovary"
+New-Item "$ovary" -type directory | Out-Null
+if (!(Test-Path "$womb")) {
+  New-Item "$womb" -type directory | Out-Null
+}
+
+Write-Host "downloading seed..."
 (new-object System.Net.WebClient).DownloadFile("https://nodeload.github.com/scarytom/ovum/zipball/$parent","$ovary\data.zip")
 
+Write-Host "germinating seed..."
 $shell_app=new-object -com shell.application
 $zip_file = $shell_app.namespace("$ovary\data.zip")
 $destination = $shell_app.namespace("$ovary")
-$destination.Copyhere($zip_file.items())
+$destination.Copyhere($zip_file.items(), 0x4)
 
-Write-Host $ovary
+Write-Host "removing husk..."
+Remove-Item "$ovary\data.zip"
+
+Write-Host "planting seed..."
+Move-Item "$ovary\*\*" "$womb\" -ErrorAction SilentlyContinue
+
+Write-Host "tidying up..."
+Remove-Item "$ovary" -recurse
